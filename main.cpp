4fc0b12b6173d31a2f89d70bcad796c7b5c8e6bb
5242360 <100364230+5242360@users.noreply.github.com>
2022-03-30 16:05:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int a=0,b=1,g=0, n=0;
+    cout << "inserire un numero\n";
+    cin >> n;
+    if(n>=2){
+        for(int i=0;i<n;i++){
+            g=a+b;
+            b=a;
+            a=g;
+            cout<< g << endl;
+        }
+    }else{
+        cout <<"errore\n";
+    }
+    return 0;
 }
